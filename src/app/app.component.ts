aa7342ff70b8a863718e1a30626b87bf700e24fe
jtobelem-simplon <jtobelem@simplon.co>
2020-04-02 07:59:51 +0200

    sticky footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {FeedbackService} from "./shared/feedback/feedback.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
